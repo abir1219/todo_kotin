a08f56699ff2c5d338d82dd9d5fbf0de4a391f93
samabir <abirchanda12@gmail.com>
2024-03-29 23:49:11 +0530

    first commit

---
+++ app/src/main/java/com/example/todoapp/fragments/SplashScreenFragment.kt
@@ -0,0 +1,31 @@
+package com.example.todoapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentSplashScreenBinding
+
+class SplashScreenFragment : Fragment() {
+    private var _binding:FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSplashScreenBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
