de26936193c62f2bd741c372819723cdb367cefe
Abir <abir@nimbussystems.com>
2024-03-30 18:52:37 +0530

    code optimize

--- app/src/main/java/com/example/todoapp/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/SplashScreenFragment.kt
@@ -1,10 +1,13 @@
 package com.example.todoapp.fragments
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
 import com.example.todoapp.databinding.FragmentSplashScreenBinding
 
@@ -12,12 +15,12 @@ class SplashScreenFragment : Fragment() {
     private var _binding:FragmentSplashScreenBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+   /* override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
         }
-    }
+    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +28,11 @@ class SplashScreenFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentSplashScreenBinding.inflate(layoutInflater, container, false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.splash_to_signIn)
+        }, 3000)
+
         return binding.root
     }
 
